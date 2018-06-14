7069023f8963279ecd4c7181c302a37a9a229557
octaguzz <octaguzz@outlook.com>
2018-06-14 22:49:32 +0100

    Adicionar arquivos de projeto.

---
+++ Livro_C-Sharp - Capitulo 7 (exercicios Parte 3)/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Livro_C_Sharp___Capitulo_7__exercicios_Parte_3_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(284, 262);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ Livro_C-Sharp - Capitulo 7 (exercicios Parte 3)/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Livro_C_Sharp___Capitulo_7__exercicios_Parte_3_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+          
+        }
+
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            DataClasses1DataContext dc = new DataClasses1DataContext();
+            var lista = from Departamentos in dc.Departamentos select Departamentos;
+            foreach (Departamentos dep in lista)
+            {
+                treeView1.Nodes.Add(dep.Sigla);
+            }
+
+            DataClasses1DataContext dc2 = new DataClasses1DataContext();
+            var lista2 = from Funcionarios in dc2.Funcionarios orderby Funcionarios.Nome select Funcionarios;
+            string departamento;
+            foreach (Funcionarios func in lista2)
+            {
+                departamento = func.Departamento;
+                foreach (TreeNode noPrincipal in treeView1.Nodes)
+                {
+                    if (noPrincipal.Text == departamento)
+                    {
+                        noPrincipal.Nodes.Add(func.Nome);
+                    }
+                }
+            }
+        }
+    }
+}
